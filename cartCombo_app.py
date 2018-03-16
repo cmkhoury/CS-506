cb2d6c827fca880548809ef94734eac8f88051e9
cmkhoury <cmkhoury@wisc.edu>
2018-03-16 12:06:29 -0500

    can add to user table, reference this for adding to other tables

--- cartCombo_app.py
+++ cartCombo_app.py
@@ -17,5 +17,51 @@ def home():
 
 app.secret_key = os.urandom(12)
 
+@app.route('/addUser')
+def new_user():
+    # if not session.get('logged_in'):
+    #     return render_template('login.html')
+    # if not session.get('is_power'):
+    #     return render_template('home.html')
+    return render_template('addUser.html')
+
+# CREATE TABLE User(
+#     UID INTEGER PRIMARY KEY,
+#     Username TEXT,
+#     Password TEXT,
+#     Email TEXT,
+#     Address TEXT,
+#     FirstName TEXT,
+#     LastName TEXT,
+#     OID INTEGER,
+
+
+@app.route('/addUserData', methods = ['POST', 'GET'])
+def addUserData():
+    # if not session.get('logged_in'):
+    #     return render_template('login.html')
+    # if not session.get('is_power'):
+    #     return render_template('home.html')
+    try:
+         username = request.form['username']
+         password = request.form['password']
+         email = request.form['email']
+         aid = request.form['aid']
+         firstname = request.form['firstname']
+         lastname = request.form['lastname']
+
+         with sql.connect("data/test.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO User(Username,Password,Email,Address,FirstName, LastName) VALUES (?,?,?,?,?,?)",(username,password,email,aid,firstname,lastname))
+            con.commit()
+            msg = "Record successfully added"
+    except Exception as e:
+             con.rollback()
+             msg = e
+
+    finally:
+             return render_template("result.html", msg = msg)
+             con.close()
+
 if __name__ == '__main__':
     app.run(debug = True, threaded=True)
