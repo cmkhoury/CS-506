000c76f3ce3ddb9bf5e4afed86bfd7b258fff17e
AAAAAlina <gqnalina@gmail.com>
2018-03-21 17:13:55 -0500

    update an environmentSetup file, which should be run at the very beginning to setup the environment

---
+++ environmentSetup.py
@@ -0,0 +1,9 @@
+'''environment setup file
+This should be run at first to install all necessary packages.
+A python file is better than a shell script because it has not OS limitation.
+'''
+
+import os
+
+os.system("pip install Flask")
+os.system("pip install bcrypt")
