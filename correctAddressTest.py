a8a04e964a2a35b0d2aa8ec983dfb8b2a44a7752
jpiccirillo <piccirilloj1@gmail.com>
2018-04-10 02:13:01 -0500

    saving some of my changes

--- correctAddressTest.py
+++ correctAddressTest.py
@@ -1,19 +1,17 @@
 import requests
 import bs4
 from lxml import html
-import urllib2
 from faker import Faker
 import sqlite3 as sql
 
 def main():
-
     LOGIN_URL = "http://127.0.0.1:5000/addUserData"
-    URL = "http://127.0.0.1:5000/addUserData"
+    URL = "http://127.0.0.1:5000/map"
 
     # Generate fake users with Faker
     users = [];
     fake = Faker()
-    for i in range(0, 1):
+    for i in range(0, 1000):
         individualUser = [];
         someInfo = fake.simple_profile(sex=None) #returns simple profile  (name, email, username)
         individualUser.append(someInfo["username"])
@@ -21,14 +19,34 @@ def main():
         individualUser.append(someInfo["name"].split(" ")[0])
         individualUser.append(someInfo["name"].split(" ")[1])
         individualUser.append(someInfo["mail"])
-        individualUser.append(fake.street_address())
-        individualUser.append(fake.city())
-        individualUser.append(fake.state())
-        individualUser.append(fake.postcode())
         users.append(individualUser)
 
     for i in range(0, len(users)):
         session_requests = requests.session()
+
+        # result = urllib2.urlopen('https://fakena.me/random-real-address/').read()
+        # tree = html.fromstring(result.content)
+        # test_address = tree.xpath("//body/p/strong/text()")
+        # print(test_address)
+
+        try:
+            result = session_requests.get("https://fakena.me/random-real-address/")
+            tree = html.fromstring(result.content)
+            test_address = tree.xpath("//body/div/div/p/strong/text()")
+            print(test_address)
+            state = test_address[1].split(",")[1].strip().split(" ")[0]
+            if (state!="WI"): continue;
+            print("In WI")
+
+            city = test_address[1].split(",")[0]
+            if (city!="Madison"): continue;
+            print("In Madison")
+
+            street_address = test_address[0]
+            zipcode = test_address[1].split(",")[1].strip().split(" ")[1]
+
+        except Exception as e:
+            print(e)
 
         # Create payload
         payload = {
@@ -37,13 +55,13 @@ def main():
             "firstname": users[i][2],
             "lastname": users[i][3],
             "email": users[i][4],
-            "inputAddress": users[i][5],
-            "inputCity": users[i][6],
-            "inputState": users[i][7],
-            "inputZip": users[i][8]
+            "inputAddress": street_address,
+            "inputCity": city,
+            "inputState": state,
+            "inputZip": zipcode,
         }
+        # print(payload)
         print(users[i][2] + ", " + users[i][3] + ", " + users[i][0] + ", " + users[i][1])
-        # Add user
         session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
 
         # Create payload to test login with new user
@@ -62,6 +80,17 @@ def main():
         if "Home" in str(bucket_names): print("Logged in as " + users[i][0])
         else: print("Log in failed for " + users[i][0])
 
+        # check if address is correctly concatenated
+        test_address = "".join([street_address, ",+", city, ",+", state, ",+",  zipcode])
+        result = session_requests.get("http://127.0.0.1:5000/map")
+        tree = html.fromstring(result.content)
+        bucket_names = tree.xpath("//script/text()")
+        # print(bucket_names)
+        # print(test_address)
+
+        if(test_address in str(bucket_names)): print("Success")
+        else: print("Fail")
+
         #Check that new user exists, then delete them
         try:
             with sql.connect("data/test.db") as con:
@@ -70,9 +99,9 @@ def main():
                 for row in inserted:
                     if row[1] == users[i][0]:  print("Found " +  users[i][0])
 
-                deleted = con.execute("DELETE FROM User WHERE UID = (?)", (102,))
-                if deleted.rowcount > 0: print("Deleted " + users[i][0])
-                else: print("Delete failed")
+                # deleted = con.execute("DELETE FROM User WHERE Username = (?)", (users[i][0],))
+                # if deleted.rowcount > 0: print("Deleted " + users[i][0])
+                # else: print("Delete failed")
                 con.commit()
 
         except Exception as e:
